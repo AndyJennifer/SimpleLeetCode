f3be409b55ece69e0e63df1f5395b31504b07d20
AndyJennifer <1225868370@qq.com>
2018-01-16 22:59:15 +0800

    添加了字符串中求最大长度的字串

---
+++ src/LongestSubstring.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Author:  andy.xwt
+ * Date:    2018/1/16 22:09
+ * Description:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * <p>
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * <p>
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("vjovnhuuxtdalgjccpruzqkysmwrecsalhvaqsvnlynygybinufpnpnfejyinskybgzrywelutkctnkjhryujnbpwbxl"));
+    }
+
+    /**
+     * 第一次方法走的比较耗费时间 内存换时间，时间换内存
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+                //判断是否sub中的数据时候重复
+                Set<Character> set = new HashSet<>();
+                for (int k = 0; k < sub.length(); k++) {
+                    char c = sub.charAt(k);
+                    if (!set.contains(c)) {
+                        set.add(c);
+                    } else {
+                        break;
+                    }
+                }
+                max = Math.max(max, set.size());
+            }
+
+        }
+        return max;
+    }
+
+    /**
+     * 第二种解决方法
+     */
+    public static int soluation2(String s) {
+
+        return 0;
+    }
+
+
+}
