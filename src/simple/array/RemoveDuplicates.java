0e983a552a1d50044b92a30f03929b08065c13db
AndyJennifer <1225868370@qq.com>
2020-05-28 22:27:25 +0800

    添加了移除重复数的多种解法

--- src/simple/array/RemoveDuplicates.java
+++ src/simple/array/RemoveDuplicates.java
@@ -32,12 +32,13 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-        removeDuplicates(new int[]{1, 1, 2});
+        int i = removeDuplicates3(new int[]{0, 1, 2});
+        System.out.println(i);
 
     }
 
     /**
-     * 第一种解法就是去重
+     * 第一种解法就是去重,时间复杂度O(logN)，空间复杂度O(N),不满足空间复杂度为0(1)
      */
     public static int removeDuplicates(int[] nums) {
         Set set = new HashSet<Integer>();
@@ -55,18 +56,70 @@ public class RemoveDuplicates {
     }
 
     /**
-     * 第二中解法
+     * 第二中解法双指针（快慢指针）:
+     * <p>
+     * 首先注意数组是有序的，那么重复的元素一定会相邻。
+     * <p>
+     * 要求删除重复元素，实际上就是将不重复的元素移到数组的左侧。
+     * <p>
+     * 考虑用 2 个指针，一个在前记作 p，一个在后记作 q，算法流程如下：
+     * <p>
+     * 1.比较 p 和 q 位置的元素是否相等。
+     * <p>
+     * 如果相等，q 后移 1 位
+     * 如果不相等，将 q 位置的元素复制到 p+1 位置上，p 后移一位，q 后移 1 位
+     * 重复上述过程，直到 q 等于数组长度。
+     * <p>
+     * 返回 p + 1，即为新数组长度。
+     * <p>
+     * 时间复杂度为0(N),空间复杂度为0(1),
      */
     public int removeDuplicates2(int[] nums) {
-        int index = -1;
-        int val = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[++index] = nums[i];
-                val = nums[i];
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-        return index + 1;
+        return i + 1;
     }
 
+    /**
+     * 第三种解法，逻辑上更清楚
+     */
+    public static int removeDuplicates3(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];//如果数组都是不相同的，nums = [0,1,2],那么就会原地覆盖一次
+            }
+            j++;
+        }
+        return i + 1;
+    }
+
+    /**
+     * 方法4减少了原地覆盖的次数，
+     */
+    public static int removeDuplicates4(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                //这里就保证了减少复制的次数
+                if (j - i > 1) {
+                    nums[i] = nums[j];
+                }
+                i++;
+            }
+            j++;
+        }
+        return i + 1;
+    }
 }
